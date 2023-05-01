396a4031fa8f566a266038512118c52993829158
Sergey <cydoor88@gmail.com>
2023-05-01 19:47:04 +0300

    add styles

--- src/components/Menu/MainMenu.vue
+++ src/components/Menu/MainMenu.vue
@@ -1,5 +1,8 @@
 <template>
-    <i class="bi bi-list mobile-nav-toggle d-lg-none"></i>
+    <i 
+        class="bi bi-list mobile-nav-toggle d-lg-none"
+        :click="mobileNavClick"
+    />
 
     <header id="header" class="d-flex flex-column justify-content-center">
         <nav id="navbar" class="navbar nav-menu">
@@ -15,6 +18,20 @@
 </template>
 
 <script setup>
-import mainMenu from "@/models/mainMenu";
+import {ref, computed} from 'vue';
+import mainMenu from '@/models/mainMenu';
 import MenuItem from '@/components/Menu/MenuItem.vue';
+
+const mobileNavClick = () => {
+    console.log('Hello world!');
+    //document.body.classList.toggle('mobile-nav-active');
+};
+
+/*
+on('click', '.mobile-nav-toggle', function(e) {
+    select('body').classList.toggle('mobile-nav-active')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+})
+*/
 </script>
--- src/components/Talents.vue
+++ src/components/Talents.vue
@@ -7,6 +7,21 @@
                 <p>{{ $t('talents.description') }}</p>
             </div>
 
+            <div class="row">
+                <div class="col-lg-4">
+                    <img src="@/assets/images/talents_skills.png" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-8 pt-4 pt-lg-0 content">
+                    <h3>{{ $t('talents.subtitle') }}</h3>
+
+                    <p class="fst-italic">
+                        {{ $t('talents.project_title') }}
+                    </p>
+
+                </div>
+            </div>
+
         </div>
     </section>
 </template>
